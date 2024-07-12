5848089e9fdbe5c57f962286b555abd6352244fa
rupaverma <rupaverma2409@gmail.com>
2024-07-12 07:31:27 +0530

    made some changes on pivot element

--- DataStructure/BinarySearch/pivot_element.cpp
+++ DataStructure/BinarySearch/pivot_element.cpp
@@ -9,9 +9,9 @@ int findPivotElement(vector<int> arr){
     int end = arr.size() - 1;
     int mid = start + (end - start)/2;
     while(start <= end){
-        if(arr[mid] > arr[mid + 1])
+        if(mid +1 >= arr.size() && arr[mid] > arr[mid + 1])
             return mid;
-        if(arr[mid - 1] > arr[mid])
+        if(mid - 1 >= arr[0] && arr[mid - 1] > arr[mid])
             return mid-1;
         if(arr[start] > arr[mid]){
             end = mid - 1;
